6d5d604381719fb994f403ca1c5e89ce7c218d11
cvaldovinos <54999359+cvaldovinos@users.noreply.github.com>
2023-02-24 07:05:53 +0000

    Completed Part 2

--- src/main/scala/machines/Internal.scala
+++ src/main/scala/machines/Internal.scala
@@ -5,4 +5,13 @@ import dfa._
 import scala.Conversion
 import machines.given
 
+// Conversion for Part 1
 given Conversion[Char, Character] = Character(_)
+
+// Conversion for Part 2
+given Conversion[String, RegularLanguage] with
+    def apply(s: String): RegularLanguage = 
+        s.length() match
+            case 0 => Empty
+            case 1 => Character(s.head)
+            case otherwise => Concat(s.head, s.tail)
