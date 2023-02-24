5cc169c8c728edc8ae2d3c87d1756e84a522c4be
cvaldovinos <54999359+cvaldovinos@users.noreply.github.com>
2023-02-24 08:19:30 +0000

    Completed Part 3

--- src/main/scala/machines/Internal.scala
+++ src/main/scala/machines/Internal.scala
@@ -4,9 +4,10 @@ import regex._
 import dfa._
 import scala.Conversion
 import machines.given
+import machines._
 
 // Conversion for Part 1
-given Conversion[Char, Character] = Character(_)
+given Conversion[Char, RegularLanguage] = Character(_)
 
 // Conversion for Part 2
 given Conversion[String, RegularLanguage] with
@@ -15,3 +16,14 @@ given Conversion[String, RegularLanguage] with
             case 0 => Empty
             case 1 => Character(s.head)
             case otherwise => Concat(s.head, s.tail)
+
+// Operator implementation for Part 3
+extension(lang1: RegularLanguage)
+    def ||(lang2: RegularLanguage) = Union(lang1, lang2)
+    def ~(lang2: RegularLanguage) = Concat(lang1, lang2)
+    def <*> = Star(lang1)
+    def <+> = Concat(lang1, Star(lang1))
+    def apply(n: Int): RegularLanguage = 
+        if (n == 0) then Empty
+        else if (n == 1) then lang1
+        else Concat(lang1, (lang1(n-1)))
